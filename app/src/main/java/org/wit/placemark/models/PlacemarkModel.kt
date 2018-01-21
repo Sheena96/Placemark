421bcd4111c15f4b56350973f78d03b5c5ec8217
Sheena Kelly <sheena.96@hotmail.com>
2018-01-21 20:14:14 +0000

    exercises included

--- app/src/main/java/org/wit/placemark/models/PlacemarkModel.kt
+++ app/src/main/java/org/wit/placemark/models/PlacemarkModel.kt
@@ -4,4 +4,6 @@ package org.wit.placemark.models
  * updated by sheenakelly on 17/01/2018.
  */
 
-data class PlacemarkModel(var title: String = "")
+data class PlacemarkModel(var title: String = "", var description: String = "")
+
+
--- app/src/main/java/org/wit/placemarks/activities/PlacemarkActivity.kt
+++ app/src/main/java/org/wit/placemarks/activities/PlacemarkActivity.kt
@@ -3,14 +3,14 @@ package org.wit.placemarks.activities
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_placemark.*
-import org.jetbrains.anko.AnkoLogger
-import org.jetbrains.anko.info
-import org.jetbrains.anko.toast
+import org.jetbrains.anko.*
 import org.wit.placemark.R
 import org.wit.placemark.models.PlacemarkModel
 
 class PlacemarkActivity : AppCompatActivity(), AnkoLogger
 {
+    // Creating an array list of Placemarks, and logging them
+    val placemarks = ArrayList<PlacemarkModel>()
 
     var placemark = PlacemarkModel()
 
@@ -20,12 +20,45 @@ class PlacemarkActivity : AppCompatActivity(), AnkoLogger
 
         btnAdd.setOnClickListener() {
             placemark.title = placemarkTitle.text.toString()
+            placemark.description = description.text.toString()
+            placemarks.add(placemark.copy())
+            for (i in placemarks){
+                info(i);
+            }
             if (placemark.title.isNotEmpty()) {
-                info("add Button Pressed: $placemarkTitle")
+                info("add Button Pressed: $placemark")
             }
             else {
                 toast ("Please Enter a title")
             }
+            if (placemark.description.isNotEmpty()) {
+                info("add Button Pressed: $placemark")
+            }
+            else {
+                toast ("Please Enter a description")
+            }
+            /*toast("Hi there!")
+            toast(R.string.message)
+            longToast("Wow, such duration")*/
+
+            /*snackbar(view, "Hi there!")
+            snackbar(view, R.string.message)
+            longSnackbar(view, "Wow, such duration")
+            snackbar(view, "Action, reaction", "Click me!") { doStuff() }*/
+
+            /*alert("Hi, I'm Roy", "Have you tried turning it off and on again?") {
+                yesButton { toast("Oh…") }
+                noButton {}
+            }.show()*/
+
+            /*val countries = listOf("Russia", "USA", "Japan", "Australia")
+            selector("Where are you from?", countries, { dialogInterface, i ->
+                toast("So you're living in ${countries[i]}, right?")
+            })*/
+
+           /* val dialog = progressDialog(message = "Please wait a bit…", title = "Fetching data")*/
+
         }
+
     }
 }
